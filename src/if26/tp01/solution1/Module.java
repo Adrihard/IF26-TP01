418d705546b0c97e7966353420956d23a4684561
Adrian KALMAR <adrian.kalmar@hotmail.com>
2018-09-17 17:04:51 +0200

    Created class Module.

---
+++ src/if26/tp01/solution1/Module.java
@@ -0,0 +1,72 @@
+package if26.tp01.solution1;
+
+/**
+ *
+ * @author adrian
+ */
+public class Module
+{
+    private String  sigle;
+    private String  categorie;
+    private String  parcours;
+    private int     credit;
+    
+    public Module()
+    {
+        this.sigle      = "";
+        this.categorie  = "";
+        this.parcours   = "";
+        this.credit     = 0;
+    }
+
+    public void affiche()
+    {
+        System.out.println(this.toString());
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Module{" + "sigle=" + sigle + ", categorie=" + categorie + ", parcours=" + parcours + ", credit=" + credit + '}';
+    }
+
+    public String getSigle()
+    {
+        return sigle;
+    }
+
+    public void setSigle(String sigle)
+    {
+        this.sigle = sigle;
+    }
+
+    public String getCategorie()
+    {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie)
+    {
+        this.categorie = categorie;
+    }
+
+    public String getParcours()
+    {
+        return parcours;
+    }
+
+    public void setParcours(String parcours)
+    {
+        this.parcours = parcours;
+    }
+
+    public int getCredit()
+    {
+        return credit;
+    }
+
+    public void setCredit(int credit)
+    {
+        this.credit = credit;
+    }
+}
